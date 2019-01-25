cfc78c305578c26c57ce016d58e9ca70253fe850
AlexandrMihailovich <alexsandrse@gmail.com>
2019-01-25 21:30:08 +0300

    up

---
+++ build/precache-manifest.1d9e8557949f2b4b2a114b6afcd7e283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0884ccdcb349d0d1b597",
+    "url": "/static/js/main.0884ccdc.chunk.js"
+  },
+  {
+    "revision": "b40d706c2d0de372e103",
+    "url": "/static/js/1.b40d706c.chunk.js"
+  },
+  {
+    "revision": "0884ccdcb349d0d1b597",
+    "url": "/static/css/main.c65eca52.chunk.css"
+  },
+  {
+    "revision": "b40d706c2d0de372e103",
+    "url": "/static/css/1.f6fabe9e.chunk.css"
+  },
+  {
+    "revision": "68326093263452d19aef971a476982b6",
+    "url": "/index.html"
+  }
+];
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Button, Form, Input, InputGroupAddon, InputGroup} from 'reactstrap';
+import {Button, Form, Input, DropdownToggle, InputGroupAddon, InputGroup, DropdownItem, InputGroupButtonDropdown, DropdownMenu} from 'reactstrap';
 
 class Filter extends Component {
     static defaultProps = {
@@ -12,9 +12,13 @@ class Filter extends Component {
             data: props.data,
             value: '',
             filterKeys: ['id', 'firstName', 'lastName', 'email', 'phone'],
+            splitButtonOpen: false,
+            filterCol: 'all'
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.toggleSplit = this.toggleSplit.bind(this);
+        this.selectCol = this.selectCol.bind(this);
     }
     handleSubmit(event) {
         let data = this.dataFilter(this.state.value);
@@ -25,8 +29,12 @@ class Filter extends Component {
         this.setState({value: event.target.value});
     }
     dataFilter(val) {
+        let keys = this.state.filterKeys;
+        if (this.state.filterCol !== 'all')  {
+            keys = [this.state.filterCol];
+        }
         let filter = this.state.data.filter((item) => {
-            let result = this.state.filterKeys.filter((key, index) => {
+            let result = keys.filter((key, index) => {
                 if(item[key].toString().toLowerCase().substring(0, val.length) === val.toLowerCase()) {
                     return item;
                 }
@@ -40,14 +48,35 @@ class Filter extends Component {
         return filter;
     }
 
+    toggleSplit() {
+        this.setState({
+            splitButtonOpen: !this.state.splitButtonOpen
+        });
+    }
+
+    selectCol(item) {
+        this.setState({
+            filterCol: item
+        });
+    }
+
     render() {
+        let keys = ['all'].concat(this.state.filterKeys);
         return (
             <Form onSubmit={this.handleSubmit}>
                 <InputGroup>
+                    <InputGroupButtonDropdown addonType="prepend" isOpen={this.state.splitButtonOpen} toggle={this.toggleSplit}>
+                        <DropdownToggle caret>
+                            {this.state.filterCol}
+                        </DropdownToggle>
+                        <DropdownMenu>
+                        {keys.map((item, index) => (
+                            <DropdownItem onClick={ () => this.selectCol(item) }>{item}</DropdownItem>
+                        ))}
+                        </DropdownMenu>
+                    </InputGroupButtonDropdown>
                     <Input type="text" id="filter" value={this.state.value} onChange={this.handleChange} placeholder="Search" />
-                    <InputGroupAddon addonType="append">
-                        <Button color="secondary">Search</Button>
-                    </InputGroupAddon>
+                    <InputGroupAddon addonType="append"><Button color="secondary">Search</Button></InputGroupAddon>
                 </InputGroup>
             </Form>
         );
