49406b022dbf0b0172e06dc6af083df2716bc9c5
AlexandrMihailovich <alexsandrse@gmail.com>
2019-01-25 21:37:31 +0300

    up

---
+++ build/precache-manifest.1f1acf1addcf96a10630edeb2b99b5f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "718e1d77e8a83794bd22",
+    "url": "/static/js/main.718e1d77.chunk.js"
+  },
+  {
+    "revision": "b40d706c2d0de372e103",
+    "url": "/static/js/1.b40d706c.chunk.js"
+  },
+  {
+    "revision": "718e1d77e8a83794bd22",
+    "url": "/static/css/main.c65eca52.chunk.css"
+  },
+  {
+    "revision": "b40d706c2d0de372e103",
+    "url": "/static/css/1.f6fabe9e.chunk.css"
+  },
+  {
+    "revision": "6dbae27dc71e797131c25d35d1a31052",
+    "url": "/index.html"
+  }
+];
